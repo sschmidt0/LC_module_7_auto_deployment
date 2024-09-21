10d16705c259311917f13146ece163e7cf8a4862
sakschmidt <sarah.schmidt@code.seat>
2024-09-21 21:04:04 +0200

    init repo

---
+++ README.md
@@ -0,0 +1,4 @@
+# Automatic deployment
+
+- Queremos que cada vez que se haga un merge a la rama principal de este ejercicio, se dispare un flujo de build y despliegue.
+- Usar Github Actions para esto.
---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Automatic deployment</h1>
+      <ul style={{ listStyle: "none" }}>
+        <li>
+          Queremos que cada vez que se haga un merge a la rama principal de este
+          ejercicio, se dispare un flujo de build y despliegue.
+        </li>
+        <li>Usar Github Actions para esto.</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
